50e3348093df2b3a687a486fe44fd328bfabcdc1
Alexander Golofeev <alex00x6@gmail.com>
2016-08-23 19:54:54 +0300

    filemanager (not complete)

---
+++ src/FileManager.java
@@ -0,0 +1,19 @@
+public class FileManager {
+    public static void createFile() {
+
+    }
+
+    public static void deleteFile() {
+
+    }
+
+    public static void renameFile() {
+
+    }
+    public static void findWord(){
+
+    }
+    public static void replaceWord() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Create file");
+        System.out.println("2. Delete file");
+        System.out.println("3. Rename file");
+        System.out.println("4. Find word");
+        System.out.println("5. Replace word");
+    }
+}
